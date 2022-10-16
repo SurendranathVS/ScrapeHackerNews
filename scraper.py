4a82de6c28cdf20cd89fcb9227a3ec0b7c4f7a0b
SurendranathVS <mail4surendranath@gmail.com>
2022-10-16 18:56:03 +0530

    Adding new feature: now we can scrape as many pages as we want !!

--- scraper.py
+++ scraper.py
@@ -1,19 +1,26 @@
+from re import sub
 from bs4 import BeautifulSoup
 import requests
 
 url = 'https://news.ycombinator.com/'
-res = requests.get(url)
 
-soup = BeautifulSoup(res.text, 'html.parser')
+def GetSiteData(url):
 
-titleline = soup.select('.titleline')
-subtext = soup.select('.subtext')
+    res = requests.get(url)
+
+    soup = BeautifulSoup(res.text, 'html.parser')
+
+    titleline = soup.select('.titleline')
+    subtext = soup.select('.subtext')
+    
+    return (titleline,subtext)
 
 def GetImportantArticlesSorted(hn):
     return sorted(hn, key=lambda k:k['vote'], reverse=True)
 
 
-def GetImportantArticles(titleline,subtext):
+def GetImportantArticles(url):
+    titleline,subtext = GetSiteData(url)
     hn = []
     for idx, item in enumerate(titleline):
         link = item.find('a').get('href')
@@ -25,7 +32,23 @@ def GetImportantArticles(titleline,subtext):
                 hn.append({'article':article_name, 'link':link, 'vote':points})
     return GetImportantArticlesSorted(hn)
 
+page = 1
 
+articles = GetImportantArticles(url)
 
-print(GetImportantArticles(titleline, subtext))
+while True:
+    
+    print(articles,len(articles)) 
+
+    require_more_articles = input('Enter Yes if more articles required: ')
+
+    if require_more_articles.upper()=='YES':
+        page += 1
+        print(url + f'news?p={page}')
+        articles.extend(GetImportantArticles(url + f'news?p={page}'))
+    else:
+        print('Thank you !')
+        break
+
+                            
 
